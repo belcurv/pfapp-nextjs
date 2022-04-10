4b36ad8b6b7c78704ad863ea23a16eac7afc1bb3
Jay Schwane <jrschwane@uwalumni.com>
2022-04-10 13:43:39 -0500

    Enhance testing setup

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 Credits:
 
  * https://benjaminwfox.com/blog/tech/nextjs-setup-config-testing-linting-absolute-imports
+ * https://javascript.plainenglish.io/declarative-and-scalable-testing-with-react-testing-library-177f35f41396
--- src/pages/index.test.js
+++ src/pages/index.test.js
@@ -1,10 +1,23 @@
-import { render, screen } from '@testing-library/react'
 import Home from '@/pages/index'
+import { render } from '@testing-library/react'
+
+function renderIndex (props) {
+  const result = render(<Home {...props} />)
+
+  const Main = () => result.getByRole('main')
+
+  return {
+    Main,
+    debug: result.debug
+  }
+}
 
 describe('The Home page component', () => {
   it('should have exactly 1 main section', () => {
-    render(<Home />)
-    const main = screen.getByRole('main')
-    expect(main).toBeInTheDocument()
+    const {
+      Main
+    } = renderIndex({})
+
+    expect(Main()).toBeInTheDocument()
   })
 })
