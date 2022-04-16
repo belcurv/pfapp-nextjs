d76f95c6a4d9892e68bcd11c2d3a1f4f9ad93d58
Jay Schwane <jrschwane@uwalumni.com>
2022-04-16 01:26:00 -0500

    Fix flashing icons

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,11 @@
 import Header from '@/common/components/Header'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+import { config } from '@fortawesome/fontawesome-svg-core'
 import '@/styles/globals.css'
 import '@/styles/grid.css'
+
+// Fix Font Awesome icons initially flashing wrong size
+config.autoAddCss = false
 
 function MyApp ({ Component, pageProps }) {
   return (
