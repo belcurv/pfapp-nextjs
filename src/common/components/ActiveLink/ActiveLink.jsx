b370bb37764f4744e1a31b80e2a6720011eefae5
Jay Schwane <jrschwane@uwalumni.com>
2022-04-16 17:26:44 -0500

    Refactor to use Next Link

--- src/common/components/ActiveLink/ActiveLink.jsx
+++ src/common/components/ActiveLink/ActiveLink.jsx
@@ -1,27 +1,23 @@
+import Link from 'next/link'
 import { useRouter } from 'next/router'
 import cn from 'classnames'
 import styles from './ActiveLink.module.css'
 
-export default function ActiveLink ({ children, href, ...props }) {
+export default function ActiveLink ({ children, href, className, ...props }) {
   const router = useRouter()
 
   const isActiveLink = router.pathname === href
 
-  const handleClick = (e) => {
-    e.preventDefault()
-    router.push(href)
-  }
-
   return (
-    <a
-      {...props}
-      href={href}
-      onClick={handleClick}
-      className={cn({
-        [styles['link-active']]: isActiveLink
-      })}
-    >
-      {children}
-    </a>
+    <Link href={href}>
+      <a
+        {...props}
+        className={cn({
+          [styles['link-active']]: isActiveLink
+        }, className)}
+      >
+        {children}
+      </a>
+    </Link>
   )
 }
