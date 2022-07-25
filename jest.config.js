650dac4267a586776cf54dbcb920e98452d5795a
Jay Schwane <jrschwane@uwalumni.com>
2022-07-24 20:47:14 -0500

    Update code coverage config

--- jest.config.js
+++ jest.config.js
@@ -28,7 +28,10 @@ const config = {
   collectCoverage: false,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  // collectCoverageFrom: undefined,
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx}',
+    '!**/index.js'
+  ],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
