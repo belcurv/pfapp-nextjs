869a60976425da7287877989b272d936b121f205
Jay Schwane <jrschwane@uwalumni.com>
2022-07-29 06:49:43 -0500

    Refactor coordinate logic for readability

--- src/modules/fire-calc/Components/Chart/AreaChart.jsx
+++ src/modules/fire-calc/Components/Chart/AreaChart.jsx
@@ -6,41 +6,43 @@ const AreaChart = ({ timeSeries, chartYMax, chartConfig }) => {
   const { width, height, xMargin, yMargin, spacer } = chartConfig
   const numberOfPoints = timeSeries.length - 1
 
-  // multipliers scale the output x & y coords
+  // to scale points based on chart dimensions and X, Y
   const xMultiple = (width - xMargin - spacer) / numberOfPoints
   const yMultiple = (height - yMargin - spacer) / chartYMax
 
   // build tuples based on SVG 0,0 coordinate system
-  const formattedCoordinates = timeSeries.map(([time, value]) => [
+  const scaledTimeSeries = timeSeries.map(([time, value]) => [
     (time * xMultiple) + xMargin,
     (height - (value * yMultiple)) - yMargin
   ])
 
+  const coordinates = scaledTimeSeries
+    // render n-1 polygons because each polygon spans current -> next val
+    .slice(0, scaledTimeSeries.length - 1)
+    // generate 4 corners as X,Y pairs
+    .map(([time, value], i) => {
+      const x0 = time
+      const y0 = height - yMargin
+      const x1 = time
+      const y1 = value
+      const x2 = time + xMultiple
+      const y2 = scaledTimeSeries[i + 1][1] // the value of the next point
+      const x3 = time + xMultiple
+      const y3 = height - yMargin
+
+      // <polygon> wants a 'points' prop consisting of x,y pairs as a string
+      return `${x0},${y0} ${x1},${y1} ${x2},${y2} ${x3},${y3}`
+    })
+
   return (
     <g>
-      {formattedCoordinates
-        // render n-1 polygons because each polygon covers 2 points
-        .slice(0, formattedCoordinates.length - 1)
-        .map(([x, y], i) => {
-          // prepare x,y coordinates for 4 points
-          const x1 = x
-          const y1 = height - yMargin
-          const x2 = x
-          const y2 = y
-          const x3 = x + xMultiple
-          const y3 = formattedCoordinates[i + 1][1] // the value of the next pooint
-          const x4 = x + xMultiple
-          const y4 = height - yMargin
-
-          // <polygon> wants a 'points' prop consisting of x,y pairs as a string
-          const points = `${x1},${y1} ${x2},${y2} ${x3},${y3} ${x4},${y4}`
-          // currency value tool-tip on hover
-          const title = formatAsCurrency(timeSeries[i + 1][1])
-
-          return (
-            <Polygon key={x} points={points} title={title} />
-          )
-        })}
+      {coordinates.map((points, i) => {
+        // the un-scaled dollar value at time i; add hover tooltip
+        const title = formatAsCurrency(timeSeries[i + 1][1])
+        return (
+          <Polygon key={i} points={points} title={title} />
+        )
+      })}
     </g>
   )
 }
